63722f3a27c488866fff50a827602758023592b3
Eugenio <eugeniosoton@gmail.com>
2026-09-10 08:18:54 -0600

    Commit final lab 3, creo

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Models\Tour;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/tours-activos', function () {
+    return Tour::activos()->get();
+});
+
+Route::get('/tours-por-categoria', function () {
+    return Tour::selectRaw('categoria_id, COUNT(*) as total, AVG(precio) as precio_promedio')
+        ->groupBy('categoria_id')
+        ->get();
 });
